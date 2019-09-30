c0b22ef3faf3cf117798690ff8ce08f4b3ac41a3
Berkay <berkayulke99@gmail.com>
2019-09-30 14:19:55 +0300

    added slider to Fractal Tree

--- Fractal Tree/canvas.js
+++ Fractal Tree/canvas.js
@@ -1,24 +1,31 @@
 let max_iterataion;
 let branch_amount;
-let angle;
+let angle = 0;
 let multiplier;
 let root;
 let count = 0;
+let slider;
 function setup() {
-  createCanvas(700, 700);
-  root = new Node(width / 2, height, 100, PI / 2);
+  slider = createSlider(0, PI / 2, PI / 12, PI / 120);
+  createCanvas(1000, 700);
+  root = new Node(width / 2, height + 300, 100, PI / 2);
   // constant to play with
-  max_iterataion = 10;
+  max_iterataion = 6;
   branch_amount = 3;
-  angle = PI / 12;
   multiplier = 0.85;
 }
 
 function draw() {
-  if (count == max_iterataion) noLoop();
-  frameRate(1);
-  background(124);
-  root.show();
-  root.add_new_gen(branch_amount, angle, multiplier);
-  count++;
+  if (angle != slider.value()) {
+    background(124);
+    angle = slider.value();
+    count = 0;
+    root = new Node(width / 2, height - 50, 100, PI / 2);
+  } else if (count <= max_iterataion) {
+    frameRate(20);
+    background(124);
+    root.show();
+    root.add_new_gen(branch_amount, angle, multiplier);
+    count++;
+  }
 }
